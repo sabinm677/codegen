97179876c4b439d97c2e88ab648b82c3e46180b3
sabinm677 <52751213+sabinm677@users.noreply.github.com>
2026-02-03 23:30:45 +0545

    Update SCRIPT_URL to point to the correct script

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_URL="<SCRIPT_URL>"
+SCRIPT_URL="https://raw.githubusercontent.com/sabinm677/codegen/refs/heads/develop/setup_env.sh"
 
 curl -fsSL "${SCRIPT_URL}" -o setup.sh
 bash setup.sh
